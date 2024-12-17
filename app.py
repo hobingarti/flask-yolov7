68aec71c8443281b398d317019055bb2e98e37bf
agungkezinstiki <agungkez.instiki@gmail.com>
2024-12-18 00:10:58 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import base64
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -16,8 +17,26 @@ def home():
 @app.route("/detect", methods=['GET'])
 def detect():
     # os.system("python detect.py --weights .\weight\pillar.pt --conf 0.2 --source .\uploads\pilar-16-_JPG.rf.ccf46654b9e0c5d20e3d1b50ae9c1847.jpg --project runs/pilar")
-    os.system("python detect.py --weights .\weight\pillar.pt --conf 0.2 --source .\uploads\pilar-4-_JPG.rf.27c2c37bf9eab1626fa8e943c325b0ee.jpg --project runs/pilar --name result --exist-ok")
+    os.system("python detect.py  --weights ./weight/pillar.pt --conf 0.2  --source ./uploads/pilar-4-_JPG.rf.27c2c37bf9eab1626fa8e943c325b0ee.jpg --project runs/pilar --name result --exist-ok")
     return "Processing is done"
+
+@app.route("/send-image", methods=['POST'])
+def send_image():
+    print("Image saved as 'output_image.jpg'")
+    content = request.form['imageData']
+    fileName = request.form['imageName']
+    
+    # content = content.replace('\n', '').replace('\r', '')
+    image_data = base64.b64decode(content)
+    with open(fileName, "wb") as f:
+        f.write(image_data)
+    
+    # g = open(fileName, "w")
+    # # g.write(content)
+    # g.write(base64.b64decode(content))
+    # g.close()
+    
+    return "Image saved as "+fileName
 
 
 @app.route('/upload', methods=['POST', 'GET'])
